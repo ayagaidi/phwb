7e029bf0a64b116f04148ec95a1a9883199b5fff
ayagaidi1995@gmail.com <ayagaidi1995@gmail.com>
2026-05-24 22:59:16 +0200

    dd

--- resources/views/site/layouts/app.blade.php
+++ resources/views/site/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'صيادلة بلا حدود')</title>
+    <icon rel="shortcut icon" href="{{ asset('logo.png') }}" type="image/x-icon">
     
     <!-- Modern Tailwind via CDN for quick beautiful design -->
     <script src="https://cdn.tailwindcss.com"></script>
